cc30eeb7883369b1ea87679b8f01e219452c7e37
tapuks <david_berdiell@hotmail.com>
2021-05-05 17:33:17 +0000

    finish

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const sum = (a,b) => {
 // solo un registro en consola para nosotros.
 console.log(sum(7,3))
 
+
+function fromDollarToYen(dollar) {
+    return dollar * 109.30;
+}
+console.log(fromDollarToYen(2));
+
+
+function fromEuroToDollar(euro) {
+    return euro * 1.2;
+}
+console.log(fromEuroToDollar(2));
+
+
+function fromYenToPound(yen) {
+    return yen * 0.0066;
+}
+console.log(fromYenToPound(2));
+
+
 // exporta la función para usarla en otros archivos 
 // (similar a la palabra clave `export` cuando se usa webpack)
-module.exports = { sum };
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -9,3 +9,39 @@ test('adds 14 + 9 to equal 23', () => {
     // esperamos que la suma de esos 2 números sea 23
     expect(total).toBe(23);
 });
+
+test("One euro should be 1.206 dollars", function(){
+    // importo la funcion desde app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // utilizo la function de la forma como se espera que funcione
+    const dollars = fromEuroToDollar(3.5)
+
+    // is 1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
+    
+    // hago mi comparacion (la prueba)
+    expect(expected).toBe(dollars);
+})
+
+test ("One dollar should be 109.30 yen", function(){
+
+    const { fromDollarToYen } = require('./app.js')    
+
+    const yen = fromDollarToYen(4)
+
+    const expected = 4 * 109.30;
+
+    expect(expected).toBe(yen);
+})
+
+test ("One yen should be 0.0066 pound", function(){
+
+    const { fromYenToPound } = require('./app.js')    
+
+    const pound = fromYenToPound(5)
+
+    const expected = 5 * 0.0066;
+
+    expect(expected).toBe(pound);
+})
